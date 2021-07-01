72d78a1e70d887dd1c54e81aad3bd418f76eef9a
chetu95 <36447626+chetu95@users.noreply.github.com>
2021-07-01 21:40:40 +0530

    Create get_bit_position.c

---
+++ 26-06-2021/get_bit_position.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define GETPOS(n, pos) (n>>pos-1) & 1
+
+int main()
+{
+    unsigned int num, position;
+    printf("Enter the number: ");
+    scanf("%u", &num);
+    printf("Enter the position where the bit is to be found: ");
+    scanf("%u", &position);
+    printf("The bit present in number %u at position %u is: %d", num,position, GETPOS(num, position));
+    return 0;
+}
+
+
+/* Output
+
+Enter the number: 3
+Enter the position where the bit is to be found: 2
+The bit present in number 3 at position 2 is: 1
+
+
+Enter the number: 8
+Enter the position where the bit is to be found: 1
+The bit present in number 8 at position 1 is: 0
+
+
+Enter the number: 1
+Enter the position where the bit is to be found: 1
+The bit present in number 1 at position 1 is: 1
+
+*/
