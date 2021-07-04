58d2ad6f4b0a6588937b3712bf423928c6b81770
chetu95 <36447626+chetu95@users.noreply.github.com>
2021-07-04 16:55:01 +0530

    Update get_bit_position.c

--- 26-06-2021/get_bit_position.c
+++ 26-06-2021/get_bit_position.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define GETPOS(n, pos) (n>>pos-1) & 1
+#define GETPOS(n, pos) (n>>pos) & 1
 
 int main()
 {
@@ -17,17 +17,17 @@ int main()
 /* Output
 
 Enter the number: 3
-Enter the position where the bit is to be found: 2
-The bit present in number 3 at position 2 is: 1
+Enter the position where the bit is to be found: 1
+The bit present in number 3 at position 1 is: 1
 
 
 Enter the number: 8
-Enter the position where the bit is to be found: 1
-The bit present in number 8 at position 1 is: 0
+Enter the position where the bit is to be found: 0
+The bit present in number 8 at position 0 is: 0
 
 
 Enter the number: 1
-Enter the position where the bit is to be found: 1
-The bit present in number 1 at position 1 is: 1
+Enter the position where the bit is to be found: 0
+The bit present in number 1 at position 0 is: 1
 
 */
